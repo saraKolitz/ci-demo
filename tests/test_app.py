abc91e952c421afc935d64eaa7853293224515d1
saraKolitz <s8540476@gmail.com>
2026-02-11 00:29:43 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,7 @@
 from src.app import print_name
 import  pytest
+def test_returns_string():
+    result = print_name()
+    assert isinstance(result, str)
 def test_sk():
     assert print_name() == "Sarale Kolitz"
